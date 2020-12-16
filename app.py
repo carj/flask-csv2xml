e82975c8fbd6b9d25ea4d5c5af33518b2acb4512
carj <drjamescarr@gmail.com>
2020-12-16 16:04:34 +0000

    Allow optional namespaces for columns which contain ":"

--- app.py
+++ app.py
@@ -3,24 +3,25 @@ import csv
 import time
 import shutil
 import zipfile
+
 from pyPreservica import cvs_to_xml, csv_to_search_xml, cvs_to_cmis_xslt, cvs_to_xsd
-from flask import Flask, redirect, url_for, session, request, send_file,flash
+from flask import Flask, redirect, url_for, session, request, send_file, flash
 from werkzeug.utils import secure_filename
 from flask import render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import TextField, SelectField, HiddenField, ValidationError, RadioField,\
-    BooleanField, SubmitField, IntegerField, FormField, validators
-from wtforms.validators import Required, Length
-
+from wtforms import TextField, SelectField, HiddenField, ValidationError, RadioField, SubmitField, FormField, \
+    validators, FieldList, StringField
+from wtforms.validators import Required, Length, DataRequired
 
 NS_HELP = """
-        This will become the namespace of the XML documents, e.g. http://www.openarchives.org/OAI/2.0/oai_dc/
+        This will become the default namespace of the XML documents, e.g. http://www.openarchives.org/OAI/2.0/oai_dc/
 
         XML Namespaces are any valid URI or Uniform Resource Identifier.
         """
-ROOT_HELP = 'This is Root Element of the XML documents, e.g. dc, Metadata, Root etc '
+ROOT_HELP = 'This is Root Element of the XML documents, e.g. oai_dc, dc, Metadata, Root etc '
+
 
 class DownloadForm(FlaskForm):
     xml_button = SubmitField('Download XML Documents as ZIP File')
@@ -28,29 +29,40 @@ class DownloadForm(FlaskForm):
     search_button = SubmitField('Download Custom Search Index')
     cmis_button = SubmitField('Download Custom CMIS Transform for UA')
 
+
 class CSVUploadForm(FlaskForm):
-    namespace = TextField('XML Namespace', description=NS_HELP, validators=[Required()])
-    root_element = TextField('XML Root Element', description=ROOT_HELP, validators=[Required(), Length(max=25)])
-    cvs_file = FileField("CSV File", validators=[FileRequired(), FileAllowed(['csv'], 'CSV Files Only')], description='Make sure the Excel Spreadsheet has been saved as UTF-8 CSV')
+    root_element = TextField('The Root Element Name', description=ROOT_HELP, validators=[Required(), Length(max=25)])
+    namespace = TextField('Default Namespace For The Root Element', description=NS_HELP, validators=[Required()])
+    cvs_file = FileField("CSV File", validators=[FileRequired(), FileAllowed(['csv'], 'CSV Files Only')],
+                         description='Make sure the Excel Spreadsheet has been saved as UTF-8 CSV',
+                         render_kw={"accept": ".csv,.CSV"})
     submit_button = SubmitField('Upload Spreadsheet')
 
 
 class ColumnSelect(FlaskForm):
-    column = SelectField('Unique CSV Column', description='Select a column which will be used to name the XML files. The value of this column should be different for each row of the spreadsheet.', coerce=str)
+    column = SelectField('Unique CSV Column',
+                         description='Select a column which will be used to name the XML files. The value of this column should be different for each row of the spreadsheet.',
+                         coerce=str)
     options = list()
     options.append((".xml", "XML Convention (.xml)"))
-    options.append((".metadata", "Preservica Convention (.metadata)"))
-    xml_extenstion = RadioField(label="Select XML Naming Convention", description="Select XML Naming Convention", validators=[Required()], choices=options, default='.xml')
+    options.append((".metadata", "Preservica Convention: Compatible with SIP Creator and PUT (.metadata)"))
+    xml_extenstion = RadioField(label="Select XML Naming Convention", description="Select XML Naming Convention",
+                                validators=[Required()], choices=options, default='.xml')
+
     submit_button = SubmitField('Generate XML', render_kw={"onclick": "showcursor()"})
+
+    optional_additional_namespaces = FieldList(TextField(label="", description="", render_kw={"size": "75"}),
+                                               min_entries=0, max_entries=25)
+
 
 app = Flask('app')
 bootstrap = Bootstrap(app)
 
-
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.secret_key = SECRET_KEY
-app.config['UPLOAD_FOLDER'] = '/home/pyPreservica/mysite/static/'
+app.config['UPLOAD_FOLDER'] = 'static/'
+
 
 @app.route('/download', methods=('GET', 'POST'))
 def download():
@@ -67,18 +79,35 @@ def download():
 
     return render_template('download.html', form=form)
 
+
 @app.route('/select', methods=('GET', 'POST'))
 def select():
-
     form = ColumnSelect()
-    headers=session['HEADER']
     options = list()
+    prefixes = set()
+
+    headers = session['HEADER']
     for h in headers:
-        options.append((h,h))
-        form.column.choices=options
+        options.append((h, h))
+        # check for prefixes
+        if ":" in h:
+            head, sep, tail = h.partition(":")
+            prefixes.add(head)
+
+    for p in sorted(prefixes):
+        field = form.optional_additional_namespaces.append_entry()
+        field.label = f"xmlns:{p}"
+
+    form.column.choices = options
 
     if form.validate_on_submit():
         column = form.column.data
+
+        extra_ns = {}
+        for prefix, namespace in zip(sorted(prefixes), form.optional_additional_namespaces.entries):
+            extra_ns[prefix] = namespace.data
+
+        print(extra_ns)
 
         namespace = session['NS']
         element = session['ROOT']
@@ -86,48 +115,49 @@ def select():
 
         xml_extension = form.xml_extenstion.data
 
-
         client = session['client']
         folder = os.path.join(app.config['UPLOAD_FOLDER'], client)
 
         zipFile = os.path.join(folder, "xml.zip")
         try:
-          os.remove(zipFile)
+            os.remove(zipFile)
         except OSError:
             pass
 
-
         with zipfile.ZipFile(zipFile, 'w') as myzip:
-            for xml in cvs_to_xml(csv_file=path, root_element=element, xml_namespace=namespace, file_name_column=column, export_folder=folder):
+            for xml in cvs_to_xml(csv_file=path, root_element=element, xml_namespace=namespace, file_name_column=column,
+                                  export_folder=folder, additional_namespaces=extra_ns):
                 file_name = os.path.basename(xml)
                 if xml_extension == ".metadata":
                     head, _sep, tail = file_name.rpartition(".")
                     file_name = head + xml_extension
                 myzip.write(xml, arcname=file_name)
                 try:
-                  os.remove(xml)
+                    os.remove(xml)
                 except OSError:
-                  pass
+                    pass
 
-        search_xml = csv_to_search_xml(csv_file=path,  root_element=element, xml_namespace=namespace, title="Metadata Title", export_folder=folder)
-        xsd = cvs_to_xsd(csv_file=path,  root_element=element, xml_namespace=namespace, export_folder=folder)
-        cmis = cvs_to_cmis_xslt(csv_file=path,  root_element=element, xml_namespace=namespace, title="Metadata Title", export_folder=folder)
+        search_xml = csv_to_search_xml(csv_file=path, root_element=element, xml_namespace=namespace,
+                                       title="Metadata Title", export_folder=folder, additional_namespaces=extra_ns)
+        xsd = cvs_to_xsd(csv_file=path, root_element=element, xml_namespace=namespace, export_folder=folder,
+                         additional_namespaces=extra_ns)
+        cmis = cvs_to_cmis_xslt(csv_file=path, root_element=element, xml_namespace=namespace, title="Metadata Title",
+                                export_folder=folder, additional_namespaces=extra_ns)
 
         session['XML_ZIP'] = zipFile
         session['XSD_File'] = xsd
         session['CMIS_FILE'] = cmis
         session['SEARCH_FILE'] = search_xml
 
-
         try:
-          os.remove(path)
+            os.remove(path)
         except OSError:
-          pass
+            pass
 
         return redirect(url_for('download'))
 
-
     return render_template('choice.html', form=form)
+
 
 @app.route('/restart')
 def restart():
@@ -139,11 +169,12 @@ def restart():
             return redirect(url_for('start'))
     except KeyError:
         return redirect(url_for('start'))
+    except FileNotFoundError:
+        return redirect(url_for('start'))
 
 
 @app.route('/', methods=('GET', 'POST'))
 def start():
-
     form = CSVUploadForm()
 
     if form.validate_on_submit():
@@ -169,7 +200,6 @@ def start():
                 if os.path.isdir(os.path.join(data_folder, fi)):
                     shutil.rmtree(os.path.join(data_folder, fi))
 
-
         filename = secure_filename(f.filename)
         path = os.path.join(folder, filename)
         f.save(path)
@@ -177,18 +207,17 @@ def start():
         with open(path, encoding='utf-8-sig', newline='') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-              for header in row:
-                headers.append(header)
-              break
+                for header in row:
+                    headers.append(header)
+                break
 
         if len(headers) == 0:
-            flash("Could Not Find CSV Headers.. Please Upload A Different CSV File")
+            flash("Could Not Find CSV Headers. Please Upload A Different CSV File")
             return render_template('index.html', form=form)
 
         if " " in element:
             flash("Root Element Names Should Not Contain a Space")
             return render_template('index.html', form=form)
-
 
         session['HEADER'] = headers
         session['NS'] = namespace
@@ -199,6 +228,3 @@ def start():
         return redirect(url_for('select'))
 
     return render_template('index.html', form=form)
-
-
-
